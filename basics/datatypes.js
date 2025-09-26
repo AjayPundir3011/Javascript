2c8f03425d231109aa7645e7d560d6a1616ad5fc
Ajay <Ajay@>
2025-09-26 16:23:04 +0530

    changes

---
+++ basics/datatypes.js
@@ -0,0 +1,16 @@
+
+let isLoggedIn = false;
+
+console.log(isLoggedIn);
+
+console.log(Number.MAX_VALUE);
+
+
+let score = "123abc";
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
