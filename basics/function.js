d94121631f91c50ee6f4488205d1b80ef816bfdb
Ajay <Ajay@>
2025-09-28 19:21:47 +0530

    funtion

---
+++ basics/function.js
@@ -0,0 +1,16 @@
+function sayMyName(){
+       console.log("Ajay");
+}
+
+sayMyName();
+
+
+// function isLoggedIn(username){
+//          return `${username} is logged in`;
+// }
+
+function isLoggedIn(username){
+    return username +" is logged in";
+}
+
+console.log(isLoggedIn("Ajay"));
